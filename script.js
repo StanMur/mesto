1faa68d69022a06381cae3b50e32c5c45a59b761
Murahovskiy Stanislav <webshmeb2020@yandex.ru>
2021-04-14 23:08:13 +0300

    start

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+//console.log(morningList[0]); // Выведите первый элемент
+//console.log(morningList.length -1); // Выведите последний элемент
+
+function getFullName(employee) {
+  return
+}
+
+let employee = {
+  firstName: 'Василий',
+  lastName: 'Тёркин',
+  age: 28,
+  fullName: 'employee'
+}
+
+getFullName(employee);
+console.log(employee);
